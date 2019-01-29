cb80ec026fa77a24bbd6bdafe08cec7f921d0c7f
Derek <derek.baker89@gmail.com>
2019-01-29 19:37:13 +0000

    gets first item from api

--- router.rb
+++ router.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'httparty'
+require 'json'
 
 
 get '/' do
-	# response = HTTParty.get('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
 	erb :index
 end
 
 get '/search-books' do
-
+	response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=#{params['search-term']}")
+	obj = JSON.parse(response.body)
+	erb :results, locals: {response: obj}
 end
