1ced03ba3eed11c383d18eadc1075a2050154cd5
Derek <derek.baker89@gmail.com>
2019-01-29 20:25:21 +0000

    refactored book class

--- lib/book.rb
+++ lib/book.rb
@@ -4,15 +4,21 @@ class Book
   end
 
   def title
-	  @google_book['volumeInfo']['title']
+	  volume_info['title']
   end
 
- 	def authors
-	  @google_book['volumeInfo']['authors'].join(', ')
+  def authors
+	  volume_info['authors'].join(', ')
   end
 
   def publisher
-    @google_book['volumeInfo']['publisher']
-  end
+    volume_info['publisher']
+	end
+	
+	private
+
+	def volume_info
+		@google_book['volumeInfo']
+	end
 
 end
