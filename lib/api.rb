acf9b43514f466b22798d1d3a79d10e2038fcb31
Derek <derek.baker89@gmail.com>
2019-01-31 14:30:49 +0000

    created api class

---
+++ lib/api.rb
@@ -0,0 +1,9 @@
+class Api
+	def initialize()
+
+	end
+
+	def get_books
+
+	end
+
--- lib/book.rb
+++ lib/book.rb
@@ -4,7 +4,7 @@ class Book
   end
 
   def title
-	  volume_info['title']
+    volume_info['title']
   end
 
   def authors
